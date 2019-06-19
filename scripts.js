2aac44f97005940271fdcc7cc814de6f79c15486
farp80 <farp80@gmail.com>
2019-06-19 23:35:47 +0000

    changes

---
+++ scripts.js
@@ -0,0 +1,12 @@
+function getExcuse()
+{
+    alert('it was pressed');
+   let person = ['The dog', 'My Car', 'An accident', 'Her cat'];
+   let action = ['The dog', 'My Car', 'An accident', 'Her cat'];
+   let timeToPass = ['The dog', 'My Car', 'An accident', 'Her cat'];
+}
+
+function getNumberToExcuse()
+{
+    return Math.floor(Math.random * 4);
+}
