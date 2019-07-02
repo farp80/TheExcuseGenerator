b10a19c0952eda3595750fd9b0c01d565d809b96
farp80 <farp80@gmail.com>
2019-07-02 23:19:02 +0000

    added window.oload function

--- scripts.js
+++ scripts.js
@@ -1,16 +1,16 @@
-function getExcuse()
-{
-   let person = ['The dog', 'My car', 'The door', 'Her cat'];
-   let action = ['hid my shoes ', 'was caught in a triple accident', 'got looked', 'eat my dinner'];
-   let timeToPass = ['exactly 20 mins before leaving', 'after I left Juan house', 'in the middle of the moving', 'while you were talking to me!'];
+window.onload = () => {
+    let person = ['The dog', 'My car', 'The door', 'Her cat'];
+    let action = ['hid my shoes ', 'was caught in a triple accident', 'got looked', 'eat my dinner'];
+    let timeToPass = ['exactly 20 mins before leaving', 'after I left Juan house', 'in the middle of the moving', 'while you were talking to me!'];
 
-   var nbrToGet = parseInt(getNumberToExcuse());
+    let nbrToGet = parseInt(getNumberToExcuse());
 
-   var excuse = person[nbrToGet] + " " + action[nbrToGet] + " " + timeToPass[nbrToGet];
-   document.getElementById("excuse").innerHTML = excuse;
-}
+    let excuse = person[nbrToGet] + " " + action[nbrToGet] + " " + timeToPass[nbrToGet];
+    document.getElementById("excuse").innerHTML = excuse;
 
-function getNumberToExcuse()
-{
-    return Math.floor(Math.random() * 4);
+
+    function getNumberToExcuse()
+    {
+        return Math.floor(Math.random() * 4);
+    }
 }
